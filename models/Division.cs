c7ee1bd5338aafb3984b6d2a05da3d4e8c7a9b7f
amird55 <amir.dgn@outlook.com>
2020-05-26 17:12:21 +0300

    גרסה 1

---
+++ models/Division.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace models {
+    class Division {
+        public int      Id;
+        public string   DivisionName;
+    }
+}
---
+++ models/DivisionArray.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace models {
+    public class DivisionArray {
+        private Division[] records = new Division[20];
+        private int currPosition = 0;
+
+        // CRUD
+        public void AddRecord(string newDivision) {
+            Division newRecord = new Division();
+            newRecord.Id = this.getNextDivisionId();
+            newRecord.DivisionName = newDivision;
+            this.records[this.currPosition] = newRecord;
+            this.currPosition++;
+        }
+
+
+        private int getNextDivisionId() {
+            return this.currPosition+1;
+        }
+    }
+}
